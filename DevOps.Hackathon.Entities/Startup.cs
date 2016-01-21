45ad7763cc7f6b960f7af9b0c7510ae04519ab35
Hikmet Tuncer <hikmet.tuncer@hotmail.com>
2016-01-21 10:55:09 +0100

    det ornlig sygt!!

---
+++ DevOps.Hackathon.Entities/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DevOps.Hackathon.Entities.Startup))]
+namespace DevOps.Hackathon.Entities
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ DevOps.Hackathon.Web/Models/BierModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DevOps.Hackathon.Web.Models
+{
+    public class Bier
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+   
+}
